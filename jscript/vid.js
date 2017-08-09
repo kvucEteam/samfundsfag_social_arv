d970e73251dc574b67f5c88aa7975b4b67b04a9d
Karoline Krog Vind <kakv@Jacobs-iMac.local>
2017-08-09 16:02:18 +0200

    Små justeringer

--- jscript/vid.js
+++ jscript/vid.js
@@ -58,7 +58,7 @@ $.ajax({
 function setUpTube() {
 	//alert("sut");
 	var tag = document.createElement('script');
-	tag.src = "http://www.youtube.com/iframe_api";
+	tag.src = "https://www.youtube.com/iframe_api";
 	var firstScriptTag = document.getElementsByTagName('script')[0];
 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
